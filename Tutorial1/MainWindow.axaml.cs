5164bab96cee4a4aeaaca4aac793bad049c69478
LiYuCheng <289908342@qq.com>
2024-03-13 11:10:49 +0800

    chore: 结合 ogldev.org 中 Tutorial1，阅读代码，补充注释

--- Tutorial1/MainWindow.axaml.cs
+++ Tutorial1/MainWindow.axaml.cs
@@ -20,8 +20,12 @@ namespace Tutorial1
             this.Content = myControl;
         }
 
+        // 在 Avalonia 中，OpenGL 相关的控件可直接派生自 OpenGlControlBase，
+        // 覆写 OnOpenGlInit/OnOpenGlDeinit/OnOpenGlRender 这三个方法即可，
+        // 这三个方法的作用分别是：初始化/释放/绘制
         class MyOpenGlControl : OpenGlControlBase
         {
+            // 查看 OpenGlControlBase.cs，fb 实际上是 FrameBuffer 的缩写
             protected override void OnOpenGlInit(GlInterface gl, int fb)
             {
                 base.OnOpenGlInit(gl, fb);
@@ -34,8 +38,16 @@ namespace Tutorial1
 
             protected override void OnOpenGlRender(GlInterface gl, int fb)
             {
-                gl.ClearColor(0,0,0,1);
-                gl.Clear( GL_COLOR_BUFFER_BIT);
+                // 根据 https://ogldev.org/www/tutorial01/tutorial01.html，有
+                // The call above sets the color that will be used when clearing the framebuffer (described later).
+                // The color has four channels (RGBA) and it is specified as a normalized value between 0.0 and 1.0.
+                // ...
+                // The only thing we do in our render function is to clear the framebuffer (using the color specified above - try changing it). 
+                // 结合 https://www.cnblogs.com/1024Planet/p/5643651.html，认为
+                // 通过 GlInterface.ClearColor 设置了清除时使用的颜色，颜色通过 RGBA 进行指定
+                // 通过 GlInterface.Clear 设置了需要清除的缓冲区（可同时清空多种缓冲区，使用 | 组合），GL_COLOR_BUFFER_BIT 代表当前可写的颜色缓冲区
+                gl.ClearColor(0.0f, 1.0f, 0.0f, 0.7f);
+                gl.Clear(GL_COLOR_BUFFER_BIT);
             }
         }
     }
